7036e8b03930df81ec8260561560419ceba00d38
Matheus Felicio <matheus.felicio37@gmail.com>
2016-05-19 08:54:49 -0400

    Provision script update to install composer on Vagrant Up

--- provision.sh
+++ provision.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-apt-get -y update
-
 command -v php &>/dev/null ||
 {
 	sudo apt-get install python-software-properties
@@ -11,6 +9,7 @@ command -v php &>/dev/null ||
 	apt-get --purge autoremove -y
 }
 
+apt-get -y update
 
 command -v composer &>/dev/null ||
 {
@@ -18,6 +17,7 @@ command -v composer &>/dev/null ||
 	php -r "if (hash_file('SHA384', 'composer-setup.php') === '92102166af5abdb03f49ce52a40591073a7b859a86e8ff13338cf7db58a19f7844fbc0bb79b2773bf30791e935dbd938') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 	php composer-setup.php
 	php -r "unlink('composer-setup.php');"
+	mv composer.phar /usr/local/bin/composer
 }
 
 dpkg -s nginx &>/dev/null ||
